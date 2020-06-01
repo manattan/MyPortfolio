eca38c0ff5109222d115f677272f8c99557b449d
manattan <mogimogi0529@eis.hokudai.ac.jp>
2020-06-01 22:03:48 +0900

    fix/bug in movie

--- components/templates/movieBox.vue
+++ components/templates/movieBox.vue
@@ -9,7 +9,7 @@
     <div class="movieRelease">
       <p>release: {{ formatDate(movie.release_date) }}</p>
     </div>
-    <div class="movieComment">
+    <div class="movieComment" v-if="comments[num]">
       <p>{{ comments[num].comment }}</p>
     </div>
   </div>
