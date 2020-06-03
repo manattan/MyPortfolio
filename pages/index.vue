cf25833f5f45a7d40e19a32474c3a1b5f417a107
manattan <mogimogi0529@eis.hokudai.ac.jp>
2020-06-03 21:48:05 +0900

    fix/font,movieBox

--- pages/index.vue
+++ pages/index.vue
@@ -56,7 +56,8 @@ v-container {
   font-size: 14px;
   background-color: #bbb;
   color: #333;
-  font-family: Fira Sans, sans-serif;
+  font-family: "Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN",
+    "Hiragino Sans", Meiryo, sans-serif;
   max-width: 1410px;
   overflow: hidden;
 }
