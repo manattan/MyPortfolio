d6aafa5ad836676a96973c13a7947dc07e36424d
manattan <mogimogi0529@eis.hokudai.ac.jp>
2020-06-15 22:56:26 +0900

    fix/env

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,7 +68,8 @@ module.exports = {
     // contentful
     CTF_SPACE_ID: process.env.CTF_SPACE_ID,
     CTF_BLOG_POST_TYPE_ID: process.env.CTF_BLOG_POST_TYPE_ID,
-    CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN
+    CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN,
+    TMDB_APIKEY: process.env.TMDB_APIKEY
   },
   /*
    ** Build configuration
